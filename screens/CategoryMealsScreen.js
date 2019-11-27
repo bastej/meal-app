abec790ace5b5167d5163ff2c30f4b17d58dfbe9
bastej <siatkasebastian@gmail.com>
2019-11-27 19:09:26 +0100

    Passing down category data through navigation params

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealsScreen = props => {
+  const categoryId = props.navigation.getParams("categoryId");
+
+  const currentCategory = CATEGORIES.find(cat => cat.id === categoryId);
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen</Text>
