55562699e90425ccb57fb42cec4b5f5c40bf3463
bastej <siatkasebastian@gmail.com>
2019-11-27 21:56:53 +0100

    Moving single category tile to separated Comp and custom styling

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  StyleSheet,
+  Platform,
+} from "react-native";
+
+import PlainText from "../components/PlainText";
+
+const CategoryGridTile = ({ title, color, onPress }) => {
+  let TouchableComp = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComp = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.gridItem}>
+      <TouchableComp style={{ flex: 1 }} onPress={onPress}>
+        <View style={{ ...styles.container, backgroundColor: color }}>
+          <PlainText style={styles.categoryTitle} textWeight="bold">
+            {title}
+          </PlainText>
+        </View>
+      </TouchableComp>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  categoryTitle: {
+    fontSize: 17,
+    textAlign: "right",
+  },
+});
+
+export default CategoryGridTile;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,12 +8,14 @@ import {
 } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
         onPress={() => {
           props.navigation.navigate({
             routeName: "CategoryMeals",
@@ -22,11 +24,7 @@ const CategoriesScreen = props => {
             },
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -42,11 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 
