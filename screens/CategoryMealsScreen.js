219b1cbd841c96c1781aac153472193e831844ac
bastej <siatkasebastian@gmail.com>
2019-12-02 23:20:28 +0100

    Rendering meals list for chosen category

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = props => {
   const categoryId = props.navigation.getParam("categoryId");
 
   const currentCategory = CATEGORIES.find(cat => cat.id === categoryId);
+
+  const filteredMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  const renderMealItem = ({ item: { title } }) => (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen</Text>
-      <Button
-        title="Go to meal detail"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "MealDetail" });
-        }}
+      <FlatList
+        keyExtractor={({ id }) => id}
+        data={filteredMeals}
+        renderItem={renderMealItem}
       />
     </View>
   );
