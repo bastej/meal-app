3d9e4397b636f776ebdc0681fc7621f9a729e3f8
bastej <siatkasebastian@gmail.com>
2019-12-17 08:13:18 +0100

    Rendering fallback text for favorites meals  screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 
 import { useSelector } from "react-redux";
 
@@ -6,9 +7,18 @@ import MealList from "../components/MealList";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../components/HeaderButton";
+import PlainText from "../components/PlainText";
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(({ meals }) => meals.favoriteMeals);
+
+  if (!favMeals.length || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <PlainText>Favorites meal not found. Add some!</PlainText>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -26,6 +36,14 @@ FavoritesScreen.navigationOptions = navData => ({
       />
     </HeaderButtons>
   ),
+});
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default FavoritesScreen;
